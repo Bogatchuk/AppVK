98b1ffb96854ed7f9a34a0d9c46c5ab92084654e
Unknown <bogatchukroma2013@gmail.com>
2019-05-26 22:52:23 +0300

    Start AppVK project

---
+++ RBVK/RBVK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RBVK
+//
+//  Created by Bogatchuk Roman on 5/26/19.
+//  Copyright © 2019 Bogatchuk Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
